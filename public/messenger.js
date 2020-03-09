e9477e381e5601ee831cf4d90d1d505ad4aac319
Matthew Hylton <wehttamh11@gmail.com>
2020-03-08 23:44:20 -0600

    final version

--- public/messenger.js
+++ public/messenger.js
@@ -1,19 +1,52 @@
 var username = "";
+var rgb = "000000";
+var re = /[0-9A-Fa-f]{6}/g;
 
 $(function () {
     var socket = io();
+
     $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
+      let text = $('#m').val();
+      if (text.includes('/')) {
+        if (text.includes('/nickcolor ')) {
+          if (!re.test(text.replace('/nickcolor ', ''))) {
+            re.lastIndex = 0;
+            socket.emit('error message', text + ' is not a valid RGB value');
+          }
+          else if (text.replace('/nickcolor ', '').length !== 6) {
+            socket.emit('error message', text + ' is not a valid RGB value');
+          } else {
+            rgb = text.replace('/nickcolor ', '');
+          }
+        } else if (text.includes('/nick ')) {
+          socket.emit('new username',text.replace('/nick ', ''), username)
+          username = text.replace('/nick ', '');
+        } else {
+          socket.emit('error message', text + ' is not a valid command');
+        }
+      } else {
+        socket.emit('chat message', text);
+      }
       $('#m').val('');
       return false;
     });
-    socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', function(args){
+      $('#messages').append($('<li>').text(args[0] + " " + args[1] + " " + args[2]));
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+    socket.on('bold message', function(args){
+      $('#messages').append($('<li>').text(args[0] + " " + args[1] + " " + args[2]).css("font-weight","Bold").css("color","#"+rgb));
       window.scrollTo(0, document.body.scrollHeight);
     });
 
     socket.on('new user', function(args){
+      console.log(args[2]);
         username = args[0];
+        document.cookie = "username=" + args[0];
+        var i;
+        for (i = 0; i < args[2].length; i++) {
+          $('#messages').append($('<li>').text(args[2][i]));
+        } 
         $('#messages').append($('<li>').text(args[1]));
         window.scrollTo(0, document.body.scrollHeight);
     });
@@ -24,6 +57,11 @@ $(function () {
       for (i = 0; i < users.length; i++) {
         $('#users').append($('<li>').text(users[i]));
       } 
+    });
+
+    socket.on('error message', function(msg){
+      $('#messages').append($('<li>').text(msg).css("font-weight","Bold"));
+      window.scrollTo(0, document.body.scrollHeight);
     });
   });
 
@@ -41,3 +79,20 @@ $(document).ready(function() {
     document.onmouseup = () => document.onmousemove = document.onmouseup = null;
   }
 });
+
+function getCookie() {
+  var name = "username=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      console.log(c.substring(name.length, c.length));
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-let usernames = []
+let usernames = [];
 let userNumber = 1;
+let log = [];
+let offset = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,25 +16,57 @@ app.get('/', function(req, res){
 app.use(express.static('public'));
 
 io.on('connection', function(socket){
+
   let username = "User" + userNumber;
+  if (usernames.find(e => e === getCookie)) {
+    username = getCookie;
+  }
   userNumber++;
   console.log(username + " has connected.")
-  socket.emit('new user', [username, "Your are " + username + "."]);
+  socket.emit('new user', [username, "Your are " + username + ".", log]);
   usernames.push(username);
   io.emit('update users', usernames);
   
+
   socket.on('chat message', function(msg){
+
     let date = new Date();
     let hours = date.getHours();
     let mins = date.getMinutes();
-    let secs = date.getSeconds();
-    let ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12;
     mins = mins < 10 ? '0'+mins : mins;
-    secs = secs < 10 ? '0'+secs : secs;
-    let timestamp = hours + ":" + mins + ":" + secs + " " + ampm;
-    io.emit('chat message', timestamp + " " + msg);
+    let timestamp = hours + ":" + mins;
+
+    let logMsg = timestamp + " " + username + " " + msg;
+    if (log.length >= 200) {
+      log.shift();
+      log.push(logMsg);
+    } else {
+      log.push(logMsg);
+    }
+
+    socket.broadcast.emit('chat message', [timestamp, username, msg]);
+    socket.emit('bold message', [timestamp, username, msg]);
+  });
+
+  socket.on('error message', function(msg){
+    socket.emit('error message', msg);
+  });
+
+
+  socket.on('new username', function(user, oldUser){
+    console.log('new: ' + user + ' old: ' + oldUser);
+    if (usernames.find(e => e === user)) {
+      socket.emit('error message', user + ' is not a valid username.');
+    } else {
+      username = user;
+      console.log(username + " has been updated.")
+      socket.emit('new user', [username, "Your are " + username + "."]);
+      usernames = usernames.filter(e => e !== oldUser)
+      usernames.push(username);
+      io.emit('update users', usernames);
+    }
   });
 
   socket.on('disconnect', function(){
@@ -46,3 +80,20 @@ http.listen(port, function(){
   console.log('listening on *:' + port);
   io.emit('chat message', "uuh");
 });
+
+function getCookie() {
+  var name = "username=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      console.log(c.substring(name.length, c.length));
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
